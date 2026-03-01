4f4ff0e178fb6eed68bc8e3d5060610a4f9e706e
dapzxxx <davaumarti@gmail.com>
2026-03-01 22:57:41 +0700

    🚀 Add HTML WebView Android project
    Generated by APKforge

---
+++ app/src/main/java/com/myapp/dapz/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.myapp.dapz
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var wv: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = Color.parseColor("#00ff00")
+        }
+        wv = WebView(this)
+        wv.layoutParams = FrameLayout.LayoutParams(-1, -1)
+        wv.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            allowFileAccess = true
+            useWideViewPort = true
+            loadWithOverviewMode = true
+        }
+        wv.webViewClient = WebViewClient()
+        wv.webChromeClient = WebChromeClient()
+        setContentView(wv)
+        wv.loadUrl("file:///android_asset/index.html")
+    }
+
+    @Deprecated("Deprecated")
+    override fun onBackPressed() {
+        if (wv.canGoBack()) wv.goBack() else super.onBackPressed()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Dapz Ai Analisa Trading Forex"
+include(":app")
